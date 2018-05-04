66c6192cdd23883f39330f4a60fdd626dcd7bda5
mahmon <wayne@mahmon.com>
2018-05-04 18:31:22 +0100

    Fragments switch on click of button, login to register and back

--- app/src/main/java/com/mahmon/bookcat/MainActivity.java
+++ app/src/main/java/com/mahmon/bookcat/MainActivity.java
@@ -1,7 +1,9 @@
 package com.mahmon.bookcat;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LoginFragment loginFragment = new LoginFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, loginFragment).commit();
+    }
 
+    public void gotoRegisterScreen(View view) {
+        RegisterFragment registerFragment = new RegisterFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, registerFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 
 }
--- app/src/main/java/com/mahmon/bookcat/RegisterFragment.java
+++ app/src/main/java/com/mahmon/bookcat/RegisterFragment.java
@@ -1,19 +1,31 @@
 package com.mahmon.bookcat;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class RegisterFragment extends Fragment {
 
+    EditText ET_USER_NAME, ET_USER_EMAIL, ET_USER_PASSWORD, ET_USER_PASS_CONF;
+    String user_name, user_email, user_password, user_pass_conf;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_register, parent, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_register, container, false);
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ET_USER_NAME = view.findViewById(R.id.user_name);
+        ET_USER_EMAIL = view.findViewById(R.id.user_email);
+        ET_USER_PASSWORD = view.findViewById(R.id.user_password);
+        ET_USER_PASS_CONF = view.findViewById(R.id.user_password_confirm);
     }
+
 }
