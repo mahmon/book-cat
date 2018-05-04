e73659268e2d39cf80893c510b2275fc23b01105
mahmon <wayne@mahmon.com>
2018-05-04 16:01:41 +0100

    Fragment displays in UI hard coded in XML, not runtime changeable

---
+++ app/src/main/java/com/mahmon/bookcat/RegisterFragment.java
@@ -0,0 +1,19 @@
+package com.mahmon.bookcat;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RegisterFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_register, parent, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+    }
+}
