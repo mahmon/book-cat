1df28d4cb9260452a5eec6d18309fd32554bd9e7
mahmon <wayne@mahmon.com>
2018-05-05 08:40:26 +0100

    Removed xml onclick calls, all listeners now managed in java classes

--- app/src/main/java/com/mahmon/bookcat/LoginFragment.java
+++ app/src/main/java/com/mahmon/bookcat/LoginFragment.java
@@ -1,16 +1,41 @@
 package com.mahmon.bookcat;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class LoginFragment extends Fragment {
+
+    Button btn_goto_register_screen;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btn_goto_register_screen = view.findViewById(R.id.btn_goto_register_screen);
+        btn_goto_register_screen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoRegisterScreen();
+            }
+        });
+    }
+
+    public void gotoRegisterScreen() {
+        final FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, new RegisterFragment());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 
 }
--- app/src/main/java/com/mahmon/bookcat/MainActivity.java
+++ app/src/main/java/com/mahmon/bookcat/MainActivity.java
@@ -1,9 +1,7 @@
 package com.mahmon.bookcat;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,14 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         LoginFragment loginFragment = new LoginFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, loginFragment).commit();
-    }
-
-    public void gotoRegisterScreen(View view) {
-        RegisterFragment registerFragment = new RegisterFragment();
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container, registerFragment);
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
     }
 
 }
